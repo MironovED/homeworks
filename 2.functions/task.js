d8cf0d785876d4bb96b060f35605807b08d7a265
Evgeny Mironov <e.d.mironov@yandex.ru>
2022-06-15 00:44:54 +0300

    added task 2 funcion

---
+++ 2.functions/task.js
@@ -0,0 +1,45 @@
+// Задание 1
+function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0; 
+  for(let i = 0; i < arr.length; i++) {
+    if(i > max) {
+      max = arr[i];
+    }
+    if(i < min){
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  let avg = Number((sum / (arr.length)).toFixed(2));
+  return { min: min, max: max, avg: avg };
+  }
+
+
+// Задание 2
+function worker(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+let max = 0;
+function makeWork(arrOfArr, func) {
+  let value = 0;
+  for(let i = 0; i < arrOfArr.length; i++){
+    if(func(arrOfArr[i]) > value ){
+      value = func(arrOfArr[i]);
+    }
+  }
+  
+  return max;
+}
+
+// Задание 3
+function worker2(arr) {
+  // Ваш код
+}
